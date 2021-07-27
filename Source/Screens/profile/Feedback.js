9bbff44f4df5ca00d2cfe552e536dfd63a1504bc
Suyash Srivastava <55198615+Spirit2v@users.noreply.github.com>
2021-07-27 18:47:53 +0530

    sd

--- Source/Screens/profile/Feedback.js
+++ Source/Screens/profile/Feedback.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React,{Component} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
 import {ScrollView, TextInput} from 'react-native-gesture-handler';
 import Styles from '../../Assets/Styles/Styles';
 import Header_Component from '../../Components/Header_Component';
@@ -7,7 +7,30 @@ import Colors from '../../Utils/Colors';
 import Button from '../../Components/Button';
 import TextStyles from '../../Assets/Styles/TextStyles';
 
-const Feedback = () => {
+import axios from 'axios'
+
+
+
+
+
+export default class Feedback extends Component {
+  state={
+    Help:''
+  };
+
+ async submitHandler(){
+   const help='suyash'
+   await axios.post(
+      'http://localhost:7000/user/feedback/67',help
+        )
+        .then((res)=>{ onsole.log(res)
+        })
+        
+
+  }
+  
+  render(){
+ 
   return (
     <ScrollView style={{flex: 1, backgroundColor: '#121417'}}>
       <Header_Component Heading="Feedback" />
@@ -15,19 +38,24 @@ const Feedback = () => {
         style={[Styles.Aboutus_container, {height: 369, alignItems: 'center',marginTop:-2}]}>
         <View style={Styles.Feedback_Input}>
           <TextInput
+          onChangeText={(input)=>{this.setState({Help:input})}}
             multiline={true}
             placeholder="Give your feedback"
             placeholderTextColor={Colors.lightgrey}
             style={{fontFamily: 'Open Sans'}}
           />
-        
+    <Text>{this.state.Help}</Text>    
           <View style={{alignItems: 'center'}}>
 
         
           </View>
           
         </View>
-        <Button title="Submit" style={{alignSelf: 'center', margin: 40}} />
+       
+        <Button 
+        onPress={this.submitHandler}
+        title="Submit" style={{alignSelf: 'center', margin: 40}} />
+  
       </View>
      
       <View style={[Styles.Aboutus_Footer, {marginTop: 45}]}>
@@ -38,7 +66,5 @@ const Feedback = () => {
       </View>
 
     </ScrollView>
-      );
-};
-
-export default Feedback;
+      )
+}}
--- Source/Screens/profile/Help.js
+++ Source/Screens/profile/Help.js
@@ -1,31 +1,80 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 //import all the components we are going to use
-import { SafeAreaView, StyleSheet, View, Button, Image } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+import Colors from '../../Utils/Colors';
+import TextStyles from '../../Assets/Styles/TextStyles';
+import Arrow_button from '../../Components/Arrow_button';
+import ArrowDownward from '../../Components/ArrowDownward';
+import {
+  Menu,
+  VStack,
+  Select,
+  CheckIcon,
+  Center,
+  NativeBaseProvider,
+} from 'native-base';
+import SelectDropdown from 'react-native-select-dropdown';
+import CheckBox from 'react-native-check-box';
+import Checkbox from '../../Components/Checkbox';
+import {Touchable} from 'react-native';
 
 const Help = () => {
   const [shouldShow, setShouldShow] = useState(true);
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
-      <Button
-          title="Hide/Show Component"
-          onPress={() => setShouldShow(!shouldShow)}
-        />
+     
         {/*Here we will return the view when state is true 
         and will return false if state is false*/}
         {shouldShow ? (
+          <TouchableOpacity
+            style={styles.boxe}
+            onPress={() => setShouldShow(!shouldShow)}>
+              <View
+              style={{flexDirection:'row',justifyContent:'space-between',alignItems:'stretch'}}
+              >
+              <Text
+              style={{color:'white'}}
+              >
+                Enter name
+              </Text>
+              <ArrowDownward />
+              </View>
+   
+          </TouchableOpacity>
+        ) 
+        :
+         (
+          <TouchableOpacity
+            style={styles.boxe1}
+            onPress={() => setShouldShow(!shouldShow)}>
+            <View>
+            <Text
+              style={{color:'white'}}
+              >
+                Enter name
+              </Text>
+              <Text
+              style={{color:'white'}}
+              >
+                Enter name
+              </Text>
+              
+              
 
-          
-          <Image
-            source={{
-              uri:
-                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
-            }}
-            style={{ width: 250, height: 250 }}
-          />
-        ) : null}
-      
+            </View>
+          </TouchableOpacity>
+        )}
       </View>
     </SafeAreaView>
   );
@@ -37,6 +86,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     margin: 10,
+  },
+  boxe: {
+    width: '100%',
+    height: 53,
+    backgroundColor: Colors.blackBlue,
+    borderRadius: 10,
+
+    padding: 8,
+    shadowColor: 'white',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    marginBottom: 17,
+    elevation: 8,
+    marginBottom: 20,
+  },
+  boxe1: {
+    width: '100%',
+    height: 253,
+    backgroundColor: Colors.blackBlue,
+    borderRadius: 10,
+
+    padding: 8,
+    shadowColor: 'white',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    marginBottom: 17,
+    elevation: 8,
+    marginBottom: 20,
   },
 });
 
